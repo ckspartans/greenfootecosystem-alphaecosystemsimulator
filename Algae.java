df8d173bbb0a72a819cf953360c8ec32f889e1f8
EthanFlower <32441639+EthanFlower@users.noreply.github.com>
2017-10-12 13:15:12 -0400

    Update Algae.java

--- Algae.java
+++ Algae.java
@@ -1,5 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
+
+/**NOTES
+ * 
+ * need to have boundaries but they wont f*cking work
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
 /**
  * Write a description of class Plants here.
  * 
@@ -12,41 +28,69 @@ public class Algae extends AbstOrganism
      * Act - do whatever the Plants wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
+
     //basic default constructor, this is where I initialize all the variables that must be in this class (because it inherits from the abstract class)
     public Algae(){
-        lifeForms = new ArrayList <AbstOrganism> () ;//list of all the organsims in the game
         prey = new ArrayList <Object> ();//list of all that the types of organism can feed on
         predators = new ArrayList <Object> ();//list of all the types of organsims that the organism can be eaten by 
         trophicLevel = 0;
         age = 0;
-        lifeForms.add(this);
-        repro_energy = 25;
-        siz = (int) (0.2*energy+5);
+        lifeforms.add(this);
+        repro_energy = 40;
+        siz = 5;
         GreenfootImage [] imgs;
         health = 100;
         energy = 0;
-        speed = 20;
-        att = 50;
+        speed = 0;
+        att = 0;
         def = 50;
         mutation_rate = 0;
         MyWorld world;
+        deathCheck = true;
     }
-  
+
     public void act() 
     {
         // Add your action code here.
         //This checks if the world is being needed to be called or not
         if( world == null){
             world = (MyWorld) getWorld();
-            //lifeForms.add(this);
         } 
         //this is where I call all the functions 
         reproduce();
-        age();
         feed();
         grow();
+        age();
+        
+        //System.out.println(getX() + " " + getY());
+        for (int i = 0; i < lifeforms.size()-1; i++){
+            if (lifeforms.get(i) instanceof Algae && lifeforms.get(i) != null){
+                int xs = lifeforms.get(i).getX();
+                int ys = lifeforms.get(i).getY();
+                System.out.println("Ohhh");
+                if (xs < 20){die();}
+                else if (xs > world.getWidth()-20){die();}
+                else if (ys < 20){die();}
+                else if (ys > world.getHeight()-20){die();}
+                System.out.println("Yaaaaaa");
+            }
+        }
+       
+            
+          
+        
+        System.out.println("Lifeforms" + lifeforms.size());
     }
+    //universal timer
+    public double uniTimer(double update, double updateSpeed){
+        for(int i = 0; i <= 60; i++){
+            if (i != 60){}else{
+                update+=updateSpeed;
+            }
+        }
+        return update;
+    }
+
     //this function basically adds energy to the plant, I add 0.1 energy per frame
     public void feed(){
         //increase the energy amount
@@ -60,53 +104,82 @@ public class Algae extends AbstOrganism
     public void grow(){
         //increase the size of the image bases on the current energy
         System.out.println("Grow Not implemented");
-        siz = (int) (0.2*energy+5);
-        getImage().scale(siz,siz);
+        int sizX = getImage().getWidth();
+        int sizY = getImage().getHeight();
+        sizX = (int)(0.2*energy+5);
+        sizY = (int)(0.2*energy+5);
+        getImage().scale(sizX,sizY);
+        setImage(getImage());
+
     }
     //this program checks if the algae has enough energy to reproduce, and then creates two new algaes and kills off the old one
     public void reproduce(){
         //check tp see of their is enough energy (size??) to split
         //if yes then call the constructor for new ones and kill the last one
-        if (energy >= repro_energy){
+        if((energy >= repro_energy)){
             //if yes than 
-                //call the constructor for two new algaes and kill the original
-                    //call die for the parent after calling the constructor
-                    
-                    //first I need to add an algae to the abstorganism arraylist (so that the new algae actually exists)
-                    lifeForms.add(new Algae());
-                    //now I add the algae to the world, and let him spawn 20 pixels around the old dead algae
-                    world.addObject(lifeForms.get(lifeForms.size()-1), getX() + Greenfoot.getRandomNumber(20), getY() + Greenfoot.getRandomNumber(20));
-                    
-                    //do it again for one more algae
-                    lifeForms.add(new Algae());
-                    world.addObject(lifeForms.get(lifeForms.size()-1), getX() - Greenfoot.getRandomNumber(20), getY() - Greenfoot.getRandomNumber(20));
-                    
-                    //kill the old algae now
-                    die();
+            //call the constructor for two new algaes and kill the original
+            //call die for the parent after calling the constructor
+
+            for (int o = 0; o < 2; o++){
+                //first I need to add an algae to the abstorganism arraylist (so that the new algae actually exists)
+                lifeforms.add(new Algae());
+                //now I add the algae to the world, and let him spawn around the old dead algae
+                int x = getX() + Greenfoot.getRandomNumber(siz*2)-Greenfoot.getRandomNumber(siz*4);
+                int y = getY() + Greenfoot.getRandomNumber(siz*2)-Greenfoot.getRandomNumber(siz*4);
+                world.addObject(lifeforms.get(lifeforms.size()-1), x, y);//creates new object
+
+                //restricts x and y values to the map size
+            }
+            //kill the old algae now
+
+            die();
+
         }
         System.out.println("Reproduce Not implemented");
     }
-    //this function adds age to the algae ( I dont really use it right now, but feel free to use it)
+    //this function adds age to the algae, but accelerates it too fast. needs work
     public void age(){
         //increase age
         //and check to see if past lifespan
         //if then kill them
-        age++;
-        System.out.println("Not implemented");
+        //double rate = Greenfoot.getRandomNumber(35);//random rate of growth, allowing for varrying ages
+        age = uniTimer(age, 0.175);//incriments the the age NOTE:0.02 will incriment at 1 per second
+
+        //creates 10% odds of death when they turn 65
+        if(deathCheck == true){
+            if(age > 65){
+                int death = Greenfoot.getRandomNumber(100);
+                if(death > 10){
+                    deathCheck = false;
+                } else if(death <= 10){//10% chance of death
+                    die();
+                    //System.out.println(" This worked");
+                    deathCheck = false;
+                }
+
+            }
+        }
+        //death unavoidable after 70
+        if(age > 70){
+            die();
+        }
+        world.showText("Age" + age , 100,300);
+        System.out.println(" Not implemented");
     }
     //this function is where the algae dies 
     public void die(){
-        //???Add a corspe object???
-        
+        //???Add a corspe object??? maybe
+
         //remove them from all lists
-        lifeForms.remove(this);
-        
+        lifeforms.remove(this);
+
         //remove them from the world
         world.removeObject(this);
-        
+
         System.out.println("Not implemented");
     }
-    //you dont really need this, but if you come up with a cool idea please let me know first
+    //you dont really need this, but can evole into alage latching onto carnivores
     public void move(){
         //not needed
         System.out.println("Not implemented");
@@ -116,5 +189,9 @@ public class Algae extends AbstOrganism
         //need a way to change the specific object
         //rubber band them to have tradeoffs (age to reproduce to etc_
         System.out.println("Not implemented");
+    }
+
+    public void interact() {
+        //not implmented ...... not needed
     }
 }
