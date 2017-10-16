0e468390c6def7a11a79d871f6188ac282df21bc
Shermanator1512 <31896750+Shermanator1512@users.noreply.github.com>
2017-10-16 17:17:03 -0400

    Update AI.java

--- AI.java
+++ AI.java
@@ -15,109 +15,134 @@ public class AI
     public AbstOrganism target;
     //This object holds the animals closest predator (so that it could decide whether or not it is appropriate to flee it 
     public AbstOrganism nearestPredator;
-    //This object stores the main organism that will be doing the thinking
+    //This object stores the main organism that will be doing the thinking (we will refer the animal that we pass into each function as the 'owner')
     public AbstOrganism owner = null;
-    //This int stores the 
+    //This int stores the prey's energy, which we will use to determine how much energy the animal may recieve
     public int energyFactor;
+    //This int stores the amount of desire the owner has towards eating a prey, which we will compare when determining the best prey
     public int desireFactor = 0;
-    // public static List<AbstOrganism> targets;
     /**
      * Constructor for objects of class AI
      */
+    //The paramters of AI is the animal itself
     public AI(AbstOrganism _owner)
     {
         owner = _owner;
     }
-
+    //in this function I tie everything in together to create a tracking function
     public void track(){
-        
+        //if there are no nearby preys 
         if(owner.targets == null){
+            //get out of this function since there is nothing to even track
             return;   
         }
+        //if there are preys nearby
         if(owner.targets != null){
+            //Use the function findMostDesired to find the most desiring prey for the owner
             findMostDesired();
         }
+        //If we successfully found a target to eat
         if(target != null){
-                //owner.setLocation(owner.getX() + (target.getX()-owner.getX()) , owner.getY() + (target.getY()-owner.getY()));
+               //set the owner's energyFactor to the targets energy before the target was eaten
                 owner.energyFactor = target.energy;
+                //Turn towards the target and close in on it
                 owner.turnTowards(target.getX(),target.getY());
         }
+            //Set target to null since the target is now eaten
              target = null;   
         }
-        //owner.move(owner.speed-4);  
-        //owner.turnTowards(target.getX(), target.getY());
-       // if (target == null){
-           // owner.energyFactor = 0;
-        //}
+    //In this function I determine the most desiring target
     private void findMostDesired(){
+            //This double stores the most desiring target
             double maxDesire = 0;
+            //this int stores the index of the most desiring target
             int maxIndex = 0;
+            //Get the targets nearby the owner
             List <AbstOrganism> targets = new ArrayList<AbstOrganism>(owner.targets);
+            //This new ArrayList will store the viable targets that the owner can actually go for
             ArrayList <AbstOrganism> newTargets = new ArrayList<AbstOrganism>();
-          for (int i = 0; i< targets.size(); i++){
-              AbstOrganism o = targets.get(i);
+        //For each possible prey around the owner  
+        for (int i = 0; i< targets.size(); i++){
+            //Get that possible prey  
+            AbstOrganism o = targets.get(i);
+                //if the owner is an omnivore
                   if(owner.trophicLevel == 2){
+                      //check if the owner's trophic level is above the target's trophic level
                       if (owner.trophicLevel >= o.trophicLevel){
-                                // System.out.println("LOOOL");
-                                 //targets.remove(o);
+                                    //add this potential target to the newTargets array list
                                     newTargets.add(o);
                       }
                   }
+                  //if the owner is a carnivore
                   if(owner.trophicLevel == 3){
+                      //check if the owner's trophic level is above the target's trophic level, and that it is not a plant
                       if (owner.trophicLevel >= o.trophicLevel && o.trophicLevel != 0){
-                                // System.out.println("LOOOL");
-                                 //targets.remove(o);
+                                    //add this potential target to the newTargets array list 
                                     newTargets.add(o);
                       }
                   }
-                  if(owner.trophicLevel == 1){
+             //if the owner is a herbivore
+                  if(owner.trophicLevel == 1){    
+                      //check if the target is a plant
                       if (o.trophicLevel == 0){
-                                // System.out.println("LOOOL");
-                                 //targets.remove(o);
+                                     //add this potential target to the newTargets array list 
                                     newTargets.add(o);
                       }
                   }
           }
+        //remove any targets that couldve been null
           removeNull(newTargets);
+        //if there are any new targets
           if(newTargets != null && !newTargets.isEmpty()){
+              //for each new target
               for(int i = 0; i < newTargets.size(); i++){
+                  //get that target
                   AbstOrganism o = newTargets.get(i);
                   double tempDesire = 0;
                   removeNull(newTargets);
+                  //if that target isnt null
                 if(newTargets.get(i) != null && o != null){
                     removeNull(newTargets);
+                    //calculate the desire of that specific target
                     tempDesire = calculateDesire(o);
                 }
+                  //if that new target is more desiring than the most desiring target
                 if (tempDesire > maxDesire){
-                  //System.out.println("ookokokok");
+                  //set the new maxIndex to this new target
                    maxIndex = i;
+                    //set the maxDesire to the desire of the target that has higher desire
                   maxDesire = tempDesire;
                 }
-              }
+             }
+              //now get the most desiring prey
               target = newTargets.get(maxIndex);
           }
+            //if there are no newTargets left
           else{
+              //leave this function
               return;
-          }
-                //System.out.println(maxIndex);
-                 
-               
+          }    
     }
     public void flee(){
+        //Get the targets nearby the owner
         List <AbstOrganism> predators  = new ArrayList <AbstOrganism> (owner.targets);
         ArrayList <AbstOrganism> newPredators = new ArrayList<AbstOrganism>();
         double minDist = 1000;
         int maxIndex = 0;
+        //for each target
           for (int i = 0; i< predators.size(); i++){
+              //get that target
               AbstOrganism o = predators.get(i);
-              if (owner.trophicLevel <= o.trophicLevel){
-                            // System.out.println("LOOOL");
-                             //targets.remove(o);
+              //if the trophicLevel of the owner is less than the trophic level of the target
+              if (owner.trophicLevel < o.trophicLevel){
+                                //add this target to the predators arraylist
                                 newPredators.add(o);
               }
-             // AbstOrganism closestPredator = newPredators.getNearest();
+            
           }
+        //if there are preadators around the owner
           if(newPredators != null && !newPredators.isEmpty()){
+              //for each predator, get the distance and compare them with each other to find the closest target
               for(int i = 0; i < newPredators.size(); i++){
                   AbstOrganism o = newPredators.get(i);
                   double tempDist = (int)Math.sqrt((int)Math.pow((owner.getX() - o.getX()), 2) + (int)Math.pow((owner.getY()  - o.getY()),2));
@@ -141,11 +166,14 @@ public class AI
         }
         return s;
     }
+    
+    //this program handles calculating the desire of each target
     private double calculateDesire(AbstOrganism o){
-        //int distance = dis
         int distance = 1;
         int energy = 0;
+        //if the target is not null
         if (o != null){
+            //get the distance from that target and the enery of that target
             distance = (int)Math.sqrt((int)Math.pow((owner.getX() - o.getX()), 2) + (int)Math.pow((owner.getY()  - o.getY()),2)) + 1;
             energy = o.siz;
         }
@@ -155,51 +183,10 @@ public class AI
         }
        
          
-
+        //return the energy over the distance, which will be the desireFactor when considering the best target
       return energy/distance;
 
     }
-    
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    /*public void track(){
-
-        if (target == null) {
-            // Look for targets
-
-            List <AbstOrganism> targets = owner.targets;
-            if (!targets.isEmpty()){
-                // If possible targets seen, choose one at random.
-                //System.out.println("TARGET FOUND");
-                int choice = Greenfoot.getRandomNumber(targets.size());
-                target = targets.get(choice);
-            }
-        }
-        //target acquired. Seek!
-        else {
-            //System.out.println("TARGET IS NOT NULL");
-            System.out.println("S : " + owner.trophicLevel);
-            System.out.println("T : " + target.trophicLevel);
-            if (owner.trophicLevel > target.trophicLevel){
-                // Close 90% of distance to target.  See Achilles/Tortoise.. :wink:
-                //turnTowards(target.getX(), target.getY());
-                //move((int)Math.sqrt((int)Math.pow(target.getX(),2) + (int)Math.pow(target.getY(),2)));
-                // System.out.println("WE MADE ITTTTT");
-                owner.setLocation(owner.getX() + (target.getX()-owner.getX()) , owner.getY() + (target.getY()-owner.getY()));
-                energyFactor = target.siz;
-                //if(getX() == target.getX() && getY() == target.getY()){
-                target = null;
-
-                //}
-            }
-            else {
-                target = null;
-            }
-        }
-    }*/   
+       
 }
 
