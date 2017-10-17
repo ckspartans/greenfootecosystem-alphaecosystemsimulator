94c6e79bdded528617e7e9dbb2dc9a8e5bd5cea6
EthanFlower <32441639+EthanFlower@users.noreply.github.com>
2017-10-17 17:54:18 -0400

    Update Algae.java

--- Algae.java
+++ Algae.java
@@ -2,9 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
 
 /**NOTES
- * 
- * need to have boundaries but they wont f*cking work
- * 
+ * First Iteration Algae
+ * make bigger algae feed of of close smaller algae(reduce population growth &/or size?
  * 
  */
 
@@ -23,8 +22,8 @@ public class Algae extends AbstOrganism
 
     //basic default constructor, this is where I initialize all the variables that must be in this class (because it inherits from the abstract class)
     public Algae(){
-        prey = new ArrayList <Object> ();//list of all that the types of organism can feed on
-        predators = new ArrayList <Object> ();//list of all the types of organsims that the organism can be eaten by 
+        prey = new ArrayList <Object> ();//list of all that the types of organism can feed on (none)
+        predators = new ArrayList <Object> ();//list of all the types of organsims that the organism can be eaten by (almost everything)
         trophicLevel = 0;
         age = 0;
         lifeforms.add(this);
@@ -38,7 +37,6 @@ public class Algae extends AbstOrganism
         def = 50;
         mutation_rate = 0;
         MyWorld world;
-        deathCheck = true;
     }
 
     public void act() 
@@ -54,20 +52,21 @@ public class Algae extends AbstOrganism
         grow();
         age();
 
-        //restricts x and y values to the map size
+        //restricts algae to the map size and kills them if they are outside or on the boundaries
         if (getWorld()!=null){
             //System.out.println("Ohhh");
             System.out.println(" " + getX() + " " + getY());
-            if (getX() <= 0){die();}
-            else if (getX() >= world.getWidth()){die();}
-            else if (getY() <= 0){die();}
-            else if (getY() >= world.getHeight()){die();}
+            int bound = 5;//how far from the border they will die from
+            if (getX() <= bound){die();}
+            else if (getX() >= world.getWidth() - bound){die();}
+            else if (getY() <= bound){die();}
+            else if (getY() >= world.getHeight() - bound){die();}
             //System.out.println("Yaaaaaa");
         }
 
-        System.out.println("Lifeforms" + lifeforms.size());
+        System.out.println("Lifeforms" + lifeforms.size());//Counts
     }
-    //universal timer
+    //universal timer for incrementations of stuff
     public double uniTimer(double update, double updateSpeed){
         for(int i = 0; i <= 60; i++){
             if (i != 60){}else{
@@ -77,19 +76,19 @@ public class Algae extends AbstOrganism
         return update;
     }
 
-    //this function basically adds energy to the plant, I add 0.1 energy per frame
+    //this function basically adds energy to the plant, random amount for energy per frame
     public void feed(){
         //increase the energy amount
         //later I will probably take into acount the amount of overlap with other ogranisms
-        System.out.println("Feed Not implemented");
-        energy += (double)Greenfoot.getRandomNumber(3)/10;//makes algae grow at different rates
-        //I show the energy on the screen so I know what energy the algae is at
-        world.showText("Energy" + energy , 100,100);
+        System.out.println("Feed implemented");
+        energy += (double)Greenfoot.getRandomNumber(3)/10;//random growth rate of algae
+        //show the energy on the screen so I know what energy the algae is at if needed
+        //world.showText("Energy" + energy , 100,100);
     }
     //Here I use the energy that the algae has to grow the algae to a certain size
     public void grow(){
         //increase the size of the image bases on the current energy
-        System.out.println("Grow Not implemented");
+        System.out.println("Grow implemented");
         int sizX = getImage().getWidth();
         int sizY = getImage().getHeight();
         sizX = (int)(0.2*energy+5);
@@ -107,16 +106,15 @@ public class Algae extends AbstOrganism
             //call the constructor for two new algaes and kill the original
             //call die for the parent after calling the constructor
 
-            for (int o = 0; o < 2; o++){
+            for (int i = 0; i < 2; i++){
                 //first I need to add an algae to the abstorganism arraylist (so that the new algae actually exists)
                 lifeforms.add(new Algae());
                 //now I add the algae to the world, and let him spawn around the old dead algae
-                int x = getX() + Greenfoot.getRandomNumber(20)-Greenfoot.getRandomNumber(40);
-                int y = getY() + Greenfoot.getRandomNumber(20)-Greenfoot.getRandomNumber(40);
-
+                int range = 30;//spawn range around parent
+                int x = getX() - range + Greenfoot.getRandomNumber(range*2);
+                int y = getY() - range + Greenfoot.getRandomNumber(range*2);
                 getWorld().addObject(lifeforms.get(lifeforms.size()-1), x, y);//creates new object
 
-                
             }
             //kill the old algae now
             energy = 0;
@@ -128,20 +126,24 @@ public class Algae extends AbstOrganism
     //this function adds age to the algae, but accelerates it too fast. needs work
     public void age(){
         //increase age
+        boolean deathCheck = false;
+        if (5 > age && deathCheck == false){// if age greater than zero, set deathCheck to true one
+            deathCheck = true;
+        } 
         //and check to see if past lifespan
-        //if then kill them
-        //double rate = Greenfoot.getRandomNumber(35);//random rate of growth, allowing for varrying ages
+        //if yes then kill the
         age = uniTimer(age, 0.175);//incriments the the age NOTE:0.02 will incriment at 1 per second
-        //average lifespan is around 68
-        //creates 10% odds of death when they turn 65
+        //average lifespan is around 68 
+        //creates ???% odds of death when they turn 65
         if(deathCheck == true){
             if(age > 65){
                 int death = Greenfoot.getRandomNumber(100);
-                if(death > 10){
+                int dieChance = 10;//odds of death
+                if(death > dieChance){
                     deathCheck = false;
-                } else if(death <= 10){//10% chance of death
+                } else if(death <= dieChance){
+                    System.out.println("Old age implemented");
                     die();
-                    //System.out.println(" This worked");
                     deathCheck = false;
                 }
 
@@ -150,23 +152,23 @@ public class Algae extends AbstOrganism
         //death unavoidable after 75
         if(age > 75){
             die();
+            System.out.println("Inevitable death implemented");
         }
-        world.showText("Age" + age , 100,300);
+        //world.showText("Age" + age , 100,300);
         System.out.println("Age implemented");
     }
     //this function is where the algae dies 
     public void die(){
-        //???Add a corspe object??? maybe
-
+        //???Add a corspe object??? 
         //remove them from the world if not removed already
-        if (getWorld()!=null){
+        if (getWorld()!=null){//checks to see of they haven't been removed already
             getWorld().removeObject(this);
         }
 
         //remove them from all lists
         lifeforms.remove(this);
 
-        System.out.println("Die not implemented");
+        System.out.println("Die implemented");
     }
     //you dont really need this, but can evole into alage latching onto carnivores
     public void move(){
