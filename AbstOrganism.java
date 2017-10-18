5383ae09dc94abb7ba107b261d0fa51039b113b6
Shermanator1512 <31896750+Shermanator1512@users.noreply.github.com>
2017-10-17 23:25:37 -0400

    Add files via upload

---
+++ AbstOrganism.java
@@ -0,0 +1,72 @@
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class AbstOrgansim here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+//This class is the AbstOrganism class with the abstract properties updated
+public abstract class AbstOrganism extends Actor
+{
+    //I use trophic levels to determine which organisms can and cannot eat
+    public int trophicLevel;
+    //Age is used to determine how long an organism has been in the world for
+    public int age;
+    //Repro_energy is used to know how much energy an organism must need before being able to reproduce
+    public int repro_energy;
+    //Siz is used to manage the size of the organism
+    public int siz;
+    //Health will be used in the future iterations when I implement fighting and such, currently organisms just die when they touch each other 
+    public double health;
+    //Energy is used to keep track of the organisms energy
+    public double energy;
+    //Speed is used to determine how fast the organisms may move
+    public int speed;
+    //Attack and defence will be used in the next iteration when I implement fighting and such
+    public int att;
+    public int def;
+    //I do not use mutation, but I believe it is the mutations guy responsibility
+    public double mutation_rate;
+    MyWorld world;
+    //This list keeps track of the organisms (targets) the 'owner' organism can find
+    public List <AbstOrganism> targets;
+    
+    //This is an object of the AI class, which is neccessary so that the organims have 
+    AI thinker;
+     //This variable is used to determine how far each organism can see
+     int sight;
+     
+     double energyFactor;
+    
+     public double hungerFactor;
+    
+    public void act() 
+    {
+        // Add your action code here.
+        if(world == null){
+            world = (MyWorld) getWorld();
+        } 
+    }
+          
+    public abstract void feed();
+
+    public abstract void grow();
+        
+    public abstract void interact();
+    
+    public abstract void reproduce();
+
+    public abstract void age();
+
+    public abstract void die();
+
+    public abstract void move();
+
+    public abstract void mutate(); 
+
+    public void say(String s){
+        System.out.println(s);
+    }
+
+}
---
+++ MeatEater.java
@@ -0,0 +1,132 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class Plants here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MeatEater extends AbstOrganism
+{
+    /**
+     * Act - do whatever the Plants wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+
+    //basic default constructor, this is where I initialize all the variables that must be in this class (because it inherits from the abstract class)
+    AbstOrganism target = null;
+    //REFER TO ABSTORGANISM FOR DOCUMENTATION OF EACH VARIABLE 
+    public MeatEater(){
+        trophicLevel = 3;
+        age = 0;
+        world.carnivores.add(this);
+        repro_energy = 80;
+        siz = 50;
+        health = 100;
+        energy = 50;
+        speed = 5;
+        att = 50;
+        def = 50;
+        mutation_rate = 0;
+        MyWorld world;
+        targets = null;
+        thinker = new AI(this);
+        sight = 30;
+        energyFactor = 0;
+
+    }
+
+    public void act() 
+    {
+        // Add your action code here.
+        //This checks if the world is being needed to be called or not
+        if( world == null){
+            world = (MyWorld) getWorld();
+            //world.carnivores.add(this);
+        } 
+        //this is where I call all the functions
+        targets = getObjectsInRange(sight, AbstOrganism.class);
+        move();
+        feed();
+
+        grow();
+        reproduce();
+
+    }
+
+    public void feed(){
+        //if an carnivore is touching a prey (and it isnt algae)
+        if(this.isTouching(AbstOrganism.class) && !this.isTouching(Algae.class)){
+            //add the appropriate energy to the omnivore
+            energy += energyFactor * 0.1;
+            //remove the prey from the world
+            removeTouching(null);
+        }
+    }
+
+    public void grow(){
+        siz = (int)energy;
+        getImage().scale(siz,siz);
+    }
+    //this program checks if the carnivore has enough energy to reproduce, and then creates a new carnivore
+    public void reproduce(){
+        if (energy >= repro_energy){
+
+            world.carnivores.add(new MeatEater());
+            world.addObject(world.carnivores.get(world.carnivores.size()-1), getX() + Greenfoot.getRandomNumber(200), getY() + Greenfoot.getRandomNumber(200));
+
+            world.carnivores.add(new MeatEater());
+            world.addObject(world.carnivores.get(world.carnivores.size()-1), getX() - Greenfoot.getRandomNumber(200), getY() - Greenfoot.getRandomNumber(200));
+
+            die();
+        }
+    }
+
+    public void age(){
+
+    }
+    public void die(){
+
+        //if they ran out of energy
+
+        //???Add a corspe object???
+
+        //remove them from all lists
+        world.carnivores.remove(this);
+
+        //remove them from the world
+        world.removeObject(this);
+
+    }
+    public void move(){
+
+        //move constantly
+        move((int)speed);
+        //if there are targets nearby
+        if(targets != null && !targets.isEmpty()){
+            //track them
+            thinker.track();
+        }
+        //if there are no targets nearby
+        else if (targets == null || targets.isEmpty()) {
+            //keep moving randomly and try to find them
+            if(Greenfoot.getRandomNumber(100) < 10){
+                turn(Greenfoot.getRandomNumber(180));
+            }
+        }
+        //if your at the edge
+        if(isAtEdge()){
+            //turn in other direction
+            turn(180);
+        }
+    }
+    public void mutate(){
+
+    }
+
+    public void interact(){
+
+    }
+    public void track() {
+    }    
+}
