296f8d455e0524475838f604894ced8fb8b6b9aa
Shermanator1512 <31896750+Shermanator1512@users.noreply.github.com>
2017-11-04 19:47:18 -0400

    Update EnergyBar.java

--- EnergyBar.java
+++ EnergyBar.java
@@ -9,28 +9,35 @@ import java.io.*;
 public class EnergyBar extends Actor
 {
     GreenfootImage temp;
+    //This color object shows how much energy the organism has
     Color color; 
+    //We pass in an owner to figure out its energy and display it
     AbstOrganism owner;
+    //max health will be the amount the organism has when it spawns into the world
     int maxHealth;
     int size;
+    //the health will be how much energy it has right
     int health;
     MyWorld world;
+    //the constructor needs the owner organism, and the health before they start losing or gaining any
     public EnergyBar(AbstOrganism o, int maxHeath){
-        // color = Color(0, 255, 0);
         this.maxHealth = (int)o.health;
         owner = o;
+        //size of the bar will be half of the size owners size
         size = owner.getImage().getHeight()/2;
 
     }
 
     public void update() 
     {
+        //if the mouse is on top of the owner
         if (Greenfoot.mouseMoved(owner)){
+        //then update the health bar and show it to the user
             temp = getImage();
 
             temp.clear();
 
-            //temp.clear();
+
             size = owner.getImage().getHeight()/2;
             health = (int)owner.energy;
             if(owner != null){
@@ -39,7 +46,7 @@ public class EnergyBar extends Actor
             else{
                 world.removeObject(this);
             }
-            //update the image
+          
             if (health == maxHealth|| health <= 0){return;}
             int red = 0, green = 0;
             try{
@@ -50,6 +57,7 @@ public class EnergyBar extends Actor
             catch(Exception e){
                 System.out.println(""+red+" , "+green);
             }
+            //sometimes color could be null, to Avoid a null pointer exception i make sure its not null
             if(color != null){
                 temp.setColor(color); 
                 temp.fill();
@@ -66,6 +74,7 @@ public class EnergyBar extends Actor
             // Add your action code here.
         }
         else{
+        //if the mouse is not on top of the organism don't show the health bar
             if(temp != null){
                 temp.clear();
             }
